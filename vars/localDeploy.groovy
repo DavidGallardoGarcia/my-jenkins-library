66bba20a84aff7eb038702acb129ef641cccec9c
DavidGallardoGarcia <davidsalesianos96@gmail.com>
2024-02-09 11:56:22 +0100

    fix local deploy

--- vars/localDeploy.groovy
+++ vars/localDeploy.groovy
@@ -1,23 +1,13 @@
-def call() {
-    stage('Local Deploy') {
-        when {
-            expression { params.LOCAL_DEPLOY }
-        }
-        steps {
-            script {
-                input 'Deploy locally?'
-                def containerName = 'my-nodejs-app'
-                def containerExists = sh(script: "docker ps -a --format '{{.Names}}' | grep -q '^${containerName}\$'", returnStatus: true)
+def call(String dockerImageTag) {
+    def containerName = 'my-nodejs-app'
+    def containerExists = sh(script: "docker ps -a --format '{{.Names}}' | grep -q '^${containerName}\$'", returnStatus: true)
 
-                if (containerExists == 0) {
-                    echo "Container '${containerName}' exists"
-                    sh "docker rm -f '${containerName}'"
-                    echo "Remove container '${containerName}'"
-                }
-                
-                sh "docker run -p 3000:3000 -d --name '${containerName}' --rm ${env.DOCKER_IMAGE_TAG}"
-                echo 'Deploy Image Completed'
-            }
-        }
+    if (containerExists == 0) {
+        echo "Container '${containerName}' exists"
+        sh "docker rm -f '${containerName}'"
+        echo "Remove container '${containerName}'"
     }
+    
+    sh "docker run -p 3000:3000 -d --name '${containerName}' --rm ${dockerImageTag}"
+    echo 'Deploy Image Completed'
 }
